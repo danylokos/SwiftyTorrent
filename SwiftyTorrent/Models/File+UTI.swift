f178fe157f8415f1695efdc98dc68296dac3d0de
Danylo Kostyshyn <danylo.kostyshyn@gmail.com>
2021-06-14 16:53:33 +0300

    Use `UniformTypeIdentifiers` framework to deal with MIME types

--- SwiftyTorrent/Models/File+UTI.swift
+++ SwiftyTorrent/Models/File+UTI.swift
@@ -7,36 +7,25 @@
 //
 
 import Foundation
-import MobileCoreServices
+import UniformTypeIdentifiers
 
 extension File {
     
     private var fileExtension: String {
         return URL(fileURLWithPath: path).pathExtension
     }
-    
-    private func getMimeType() -> CFString? {
-        let fileExt = fileExtension as CFString
-        guard
-            let extUTI = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, fileExt, nil)?
-                .takeUnretainedValue(),
-            let mimeUTI = UTTypeCopyPreferredTagWithClass(extUTI, kUTTagClassMIMEType)?
-                .takeUnretainedValue()
-            else { return nil }
-        return mimeUTI
-    }
-    
+        
     func isVideo() -> Bool {
+        // Special handling for 'mkv' container
         switch fileExtension {
         case "mkv": return true
         default: break
         }
+        // Other file extensions
         guard
-            let mimeType = getMimeType(),
-            let mimeUTI = UTTypeCreatePreferredIdentifierForTag(kUTTagClassMIMEType, mimeType, nil)?
-                .takeUnretainedValue()
+            let mimeUTI = UTType(filenameExtension: fileExtension)
         else { return false }
-        return UTTypeConformsTo(mimeUTI, kUTTypeVideo)
+        return mimeUTI.conforms(to: .audiovisualContent)
     }
     
 }
