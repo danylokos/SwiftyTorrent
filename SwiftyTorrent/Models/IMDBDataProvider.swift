4cf2448c556e3c4425b9d5f961d195459869f188
Danylo Kostyshyn <danylo.kostyshyn@gmail.com>
2021-06-14 19:48:02 +0300

    Add caching for IMDB fetcher

--- SwiftyTorrent/Models/IMDBDataProvider.swift
+++ SwiftyTorrent/Models/IMDBDataProvider.swift
@@ -20,21 +20,29 @@ final class IMDBDataProvider {
     private let urlSession: URLSession = URLSession.shared
     private let endpointURL = URL(string: "https://sg.media-imdb.com/")!
     
+    private var cache = [String: String]()
+    
     func fetchSuggestions(_ query: String) -> AnyPublisher<String, Error> {
         let query = query.lowercased()
-        let prefix = String(query.prefix(1))
-        let requestURL = endpointURL.appendingPathComponent("suggests/\(prefix)/\(query).json")
+        // Cache look-up
+        if let imdbId = cache[query] {
+            return Just(imdbId)
+                .setFailureType(to: Error.self)
+                .eraseToAnyPublisher()
+        }
+        // Constructs path like: `suggests/s/simpsons.json`
+        let requestURL = endpointURL.appendingPathComponent("suggests/\(query.prefix(1))/\(query).json")
         return urlSession
             .dataTaskPublisher(for: requestURL)
-            .tryMap({ data, response -> Data in
+            .tryMap { data, response -> Data in
                 guard let httpResponse = response as? HTTPURLResponse,
-                    httpResponse.statusCode == 200 else {
-                        throw URLError(.badServerResponse)
-                }
+                      httpResponse.statusCode == 200 else {
+                          throw URLError(.badServerResponse)
+                      }
                 return data
-            })
-            .tryMap({ (data) -> Data in
-                //json-np
+            }
+            .tryMap { (data) -> Data in
+                // Strip json-p padding `imdb$search_query()`
                 var value = String(bytes: data, encoding: .utf8)!
                 if let idx = value.firstIndex(of: "(") {
                     value.removeSubrange(value.startIndex..<idx)
@@ -42,14 +50,18 @@ final class IMDBDataProvider {
                     value = String(value.dropLast())
                 }
                 return value.data(using: .utf8)!
-            })
+            }
             .decode(type: Response.self, decoder: JSONDecoder())
-            .tryMap({ imdbResponse -> String in
+            .tryMap { imdbResponse -> String in
                 guard var imdbId = imdbResponse.data.first?.id else {
                     throw "IMDB show not found."
                 }
                 imdbId = String(imdbId.dropFirst(2)) // remove "tt"
                 return imdbId
+            }
+            .handleEvents(receiveOutput: { imdbId in
+                // Cache `imdbId`
+                self.cache[query] = imdbId
             })
             .eraseToAnyPublisher()
     }
